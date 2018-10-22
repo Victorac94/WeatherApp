e14ba8e254bbfb1c7e5dd6947b1e6b95e19a7407
Victor Antunez <victorac94@hotmail.com>
2018-10-22 22:12:08 +0200

    Updates

---
+++ precache-manifest.75714fea620e957e5c5f2b74fe09c007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "11b08912461d534d033a",
+    "url": "/WeatherApp/static/js/main.11b08912.chunk.js"
+  },
+  {
+    "revision": "6506165defdb652d5cf3",
+    "url": "/WeatherApp/static/js/1.6506165d.chunk.js"
+  },
+  {
+    "revision": "11b08912461d534d033a",
+    "url": "/WeatherApp/static/css/main.09cec4f4.chunk.css"
+  },
+  {
+    "revision": "210488ec71b2a34236fd84506f1afa85",
+    "url": "/WeatherApp/index.html"
+  }
+];
